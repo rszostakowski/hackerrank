44622afb89ac01cf7da178784a2e25be32ce2d74
rszostakowski <robertszostakowski@gmail.com>
2018-11-24 01:35:46 +0100

    improving the solution

--- src/main/python/algorithms/implementation/ServiceLane.py
+++ src/main/python/algorithms/implementation/ServiceLane.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+!/bin/python3
 
 import math
 import os
@@ -6,13 +6,20 @@ import random
 import re
 import sys
 
-# Complete the serviceLane function below.
-def serviceLane(n, width, cases):
-    print("n is {} cases are {}".format(n,cases))
-    for i in range(0, n): 
+def serviceLane(n, cases, width):
+    for i in range(0, len(cases)):
         start = cases[i][0]
         end = cases[i][1]
+        # return(min(width[start:end]))
         print(min(width[start:end]))
 
-serviceLane(6,1)
-
+n = 5
+width = [2, 3, 2, 3, 2, 3, 3]
+cases = [
+    [0, 3],
+    [4, 6],
+    [6, 7],
+    [3, 5],
+    [0, 7]
+]
+serviceLane(n, cases, width)
