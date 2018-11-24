0056c730cead23a1ff4333b7bbd77bbaa87840f0
rszostakowski <robertszostakowski@gmail.com>
2018-11-24 13:01:01 +0100

    fixnigalgorithm

--- src/main/python/algorithms/implementation/ServiceLane.py
+++ src/main/python/algorithms/implementation/ServiceLane.py
@@ -1,4 +1,4 @@
-!/bin/python3
+#!/bin/python3
 
 import math
 import os
@@ -6,15 +6,19 @@ import random
 import re
 import sys
 
+
 def serviceLane(n, cases, width):
+    result = []
     for i in range(0, len(cases)):
         start = cases[i][0]
         end = cases[i][1]
-        # return(min(width[start:end]))
-        print(min(width[start:end]))
+        result.append(min(width[start:(end+1)]))
+    print(result)
+    return result
+
 
 n = 5
-width = [2, 3, 2, 3, 2, 3, 3]
+width = [2, 3, 1, 2, 3, 2, 3, 3]
 cases = [
     [0, 3],
     [4, 6],
@@ -23,3 +27,14 @@ cases = [
     [0, 7]
 ]
 serviceLane(n, cases, width)
+print("Second example")
+n2 = 5
+width2 = [1, 2, 2, 2, 1]
+cases2 = [
+    [2, 3],
+    [1, 4],
+    [2, 4],
+    [2, 4],
+    [2, 3]
+]
+serviceLane(n2, cases2, width2)
