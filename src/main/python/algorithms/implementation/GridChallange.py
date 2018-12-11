f198f9ad7cef909204ccaa18be9ed38222ab861d
rszostakowski <robertszostakowski@gmail.com>
2018-12-11 22:40:35 +0100

    working on grid solution

--- src/main/python/algorithms/implementation/GridChallange.py
+++ src/main/python/algorithms/implementation/GridChallange.py
@@ -8,21 +8,26 @@ def gridChallenge(grid):
 
 
 def checkRows(grid):
-    # TODO 
-   return True
+    for i in range(0, len(grid[0])-1):
+        sorted_row = sorted(grid[i])
+        if isNotOrdered(sorted_row):
+            return False
+    return True
 
 
 def checkColms(grid):
     for i in range(0, len(grid[0])):
         col = column(grid, i)
-        print(col)
         if isNotOrdered(col):
             return False 
     return True 
 
+
 def isNotOrdered(col):
-    # TODO
-    return False
+    print("Col : {}".format(col))
+    isSorted =  all(col[i] <= col[i+1] for i in range(len(col)-1))
+    print("isSorted {}".format(isSorted))
+    return not isSorted 
 
 def column(matrix, i):
     return [row[i] for row in matrix]
@@ -30,5 +35,5 @@ def column(matrix, i):
 grid = ['abc', 'lmp', 'qrt']
 print(gridChallenge(grid))
 
-grid =  ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
-print(gridChallenge(grid))
+# grid =  ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
+# print(gridChallenge(grid))
