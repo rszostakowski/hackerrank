5f80a08c323d5b154784fe5ae21197821e112abe
rszostakowski <robertszostakowski@gmail.com>
2018-12-12 22:00:44 +0100

    solving the solution. Going to statistics

--- src/main/python/algorithms/implementation/GridChallange.py
+++ src/main/python/algorithms/implementation/GridChallange.py
@@ -1,14 +1,21 @@
 
 def gridChallenge(grid):
-    print(grid)
+    grid = sort(grid)
     if checkRows(grid) and checkColms(grid):
         return "YES"
 
     return "NO"
 
+def sort(grid):
+    new_grid = []
+    for i in range(0, len(grid[0])):
+        sorted_row = sorted(grid[i])
+        new_grid.append(sorted_row)
+    return new_grid
+
 
 def checkRows(grid):
-    for i in range(0, len(grid[0])-1):
+    for i in range(0, len(grid[0])):
         sorted_row = sorted(grid[i])
         if isNotOrdered(sorted_row):
             return False
@@ -19,20 +26,25 @@ def checkColms(grid):
     for i in range(0, len(grid[0])):
         col = column(grid, i)
         if isNotOrdered(col):
-            return False 
-    return True 
+            return False
+    return True
 
 
 def isNotOrdered(col):
     print("Col : {}".format(col))
-    isSorted =  all(col[i] <= col[i+1] for i in range(len(col)-1))
+    isSorted = all(col[i] <= col[i+1] for i in range(len(col)-1))
     print("isSorted {}".format(isSorted))
-    return not isSorted 
+    return not isSorted
+
 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
-grid = ['abc', 'lmp', 'qrt']
+# grid = ['abc', 'lmp', 'qrt']
+# print(gridChallenge(grid))
+
+
+grid = ['kc', 'iu']
 print(gridChallenge(grid))
 
 # grid =  ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
---
+++ src/main/python/tutorials/statistics/Quartiles.py
@@ -0,0 +1,3 @@
+
+def read_quartiles():
+    print(1,2)
