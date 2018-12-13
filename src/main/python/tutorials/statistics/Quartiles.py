81cdd340c36a5cc42775275df80fb32827da621a
rszostakowski <robertszostakowski@gmail.com>
2018-12-13 20:29:21 +0100

    Workin on quantiles

--- src/main/python/tutorials/statistics/Quartiles.py
+++ src/main/python/tutorials/statistics/Quartiles.py
@@ -1,3 +1,11 @@
+import math
 
-def read_quartiles():
-    print(1,2)
+def read_quartiles(int_arr):
+    n = len(int_arr)
+    int_arr = sorted(int_arr)
+    print(int_arr[int(math.floor(n*0.25))])
+    print(int_arr[n/2])
+    print(int_arr[int(math.ceil(n*0.75))])
+
+x_arr = [3, 7, 8, 5, 12, 14, 21, 13, 18]
+read_quartiles(x_arr)
