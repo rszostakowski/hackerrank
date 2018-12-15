6e4db59aeddc463dd235afb25520e3d7b8ab0e39
rszostakowski <robertszostakowski@gmail.com>
2018-12-15 21:51:02 +0100

    another approach

--- src/main/python/tutorials/statistics/Quartiles.py
+++ src/main/python/tutorials/statistics/Quartiles.py
@@ -1,11 +1,23 @@
 import math
 
+
 def read_quartiles(int_arr):
-    n = len(int_arr)
     int_arr = sorted(int_arr)
-    print(int_arr[int(math.floor(n*0.25))])
-    print(int_arr[n/2])
-    print(int_arr[int(math.ceil(n*0.75))])
+    print(read_quantile(int_arr, 0.25))
+    print(read_quantile(int_arr, 0.5))
+    print(read_quantile(int_arr, 0.75))
+
+def read_quantile(int_arr, qunatile):
+    n = len(int_arr)
+    fraction, integer = math.modf(n*qunatile)
+    print("fraction {} integer {}".format(fraction, integer))
+    if fraction != 0.5:
+        lower = int_arr[int(integer-1)]
+        upper = int_arr[int(integer)]
+        return((lower+upper)/2)
+    else:
+        return(int_arr[int(integer)])
+
 
 x_arr = [3, 7, 8, 5, 12, 14, 21, 13, 18]
 read_quartiles(x_arr)
