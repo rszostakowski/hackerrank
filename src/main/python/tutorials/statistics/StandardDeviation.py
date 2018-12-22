d3dae0befed07cfbd131ddf336e6c57bcd9196c8
rszostakowski <robertszostakowski@gmail.com>
2018-12-22 20:32:54 +0100

    solving std ex

--- src/main/python/tutorials/statistics/StandardDeviation.py
+++ src/main/python/tutorials/statistics/StandardDeviation.py
@@ -1,7 +1,8 @@
 def calculate_std(x_arr):
-    print(x_arr)
-    pass
-
+    n = len(x_arr)
+    avg = sum(x_arr)/n
+    var = sum([((x-avg) **2) for x in x_arr])
+    return round((var/n) ** 0.5,1)
 
 x_arr = [10, 40, 30, 50, 20]
 print(calculate_std(x_arr))
