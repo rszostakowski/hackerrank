dafa8bdafe8bfcc0363589f9a58db038232fc8ad
rszostakowski <robertszostakowski@gmail.com>
2018-12-28 22:21:43 +0100

    working on normal distr task

--- src/main/python/tutorials/statistics/NormalDistribution1.py
+++ src/main/python/tutorials/statistics/NormalDistribution1.py
@@ -1,9 +1,16 @@
+import math
+
 
 def calc_prob(mi, sigma, t1, t2, t3):
-    print("t1 {} t2 {} t3 {}".format(t1, t2, t3))
+    print("mi {} sigma {} t1 {} t2 {} t3 {}".format(mi, sigma, t1, t2, t3))
+    print(cum_dist_func(mi, sigma, t1))
+    print(cum_dist_func(mi, sigma, t3)-cum_dist_func(mi, sigma, t2))
 
 
-mi, sigma = list(map(int, input().split(" ")))
-t1 = float(input().strip())
-t2, t3 = list(map(float, input().split(" ")))
-print(calc_prob(mi, sigma, t1, t2, t3))
+def cum_dist_func(mi, sigma, x):
+    return round(0.5*(1+erf((x-mi)/(sigma*math.sqrt(2)))),3)
+
+def erf(x):
+    return (2/math.sqrt(math.pi))*(math.pow(math.e, -(1/3)*math.pow(x, 3))-math.pow(math.e, 0))
+
+calc_prob(20, 2, 19.5, 20, 22)
