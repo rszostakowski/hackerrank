3c20bf0860b4199a0abaf1ebdd8d8b4f2e1eb9aa
rszostakowski <robertszostakowski@gmail.com>
2018-12-30 19:31:17 +0100

    solving normal dist 2

--- src/main/python/tutorials/statistics/NormalDistribution2.py
+++ src/main/python/tutorials/statistics/NormalDistribution2.py
@@ -1,12 +1,12 @@
 import math
-robert 
-kasia
+
 def calc_prob(mi, sigma, t1, t2):
     print("mi {} sigma {} t1 {} t2 {}".format(mi, sigma, t1, t2))
-    print()
+    print(round(100*(1-cum_dist_func(mi,sigma,80)),2))
+    print(round(100*(1-cum_dist_func(mi,sigma,60)),2))
+    print(round(100*(cum_dist_func(mi,sigma,60)),2))
 
 def cum_dist_func(mi, sigma, x):
-    return round(0.5*(1+math.erf((x-mi)/(sigma*math.sqrt(2)))),3)
-
+    return 0.5*(1+math.erf((x-mi)/(sigma*math.sqrt(2))))
 
 calc_prob(70,10,80,60)
