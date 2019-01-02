20debb505f9dd7ae86b3afc60179f0cbf9a2162a
rszostakowski <robertszostakowski@gmail.com>
2019-01-02 21:19:15 +0100

    clt3

--- src/main/python/tutorials/statistics/CentralLimitTheorem3.py
+++ src/main/python/tutorials/statistics/CentralLimitTheorem3.py
@@ -2,8 +2,12 @@ import math
 
 def calc_prob(sample_size, mi, sig, percentage,z_val):
     print("sample_size {}, mi {}, sig {}, percentage {} z value {}".format(sample_size, mi, sig, percentage, z_val))
-    print(mi-sig*(0.98))
-    print(mi+sig*0.98)
+    lower = mi-sig*1.96
+    print(lower)
+    upper = mi+sig*1.96
+    print(upper)
+    print(round(cum_dist_func(sample_size*mi,
+        math.sqrt(sample_size)*sig,upper),4))
 
 def cum_dist_func(mi, percentage, x):
     return 0.5*(1+math.erf((x-mi)/(percentage*math.sqrt(2))))
