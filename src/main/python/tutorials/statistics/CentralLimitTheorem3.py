00f38e369495f18ad7ce2d19a90ca48f2ea45c62
rszostakowski <robertszostakowski@gmail.com>
2019-01-03 22:39:01 +0100

    correlation

--- src/main/python/tutorials/statistics/CentralLimitTheorem3.py
+++ src/main/python/tutorials/statistics/CentralLimitTheorem3.py
@@ -2,14 +2,9 @@ import math
 
 def calc_prob(sample_size, mi, sig, percentage,z_val):
     print("sample_size {}, mi {}, sig {}, percentage {} z value {}".format(sample_size, mi, sig, percentage, z_val))
-    lower = mi-sig*1.96
+    lower = mi-sig*z_val/math.sqrt(sample_size)
     print(lower)
-    upper = mi+sig*1.96
+    upper = mi+sig*z_val/math.sqrt(sample_size)
     print(upper)
-    print(round(cum_dist_func(sample_size*mi,
-        math.sqrt(sample_size)*sig,upper),4))
-
-def cum_dist_func(mi, percentage, x):
-    return 0.5*(1+math.erf((x-mi)/(percentage*math.sqrt(2))))
 
 calc_prob(100,500,80,0.95,1.96)
---
+++ src/main/python/tutorials/statistics/PearsonCorrelationCoefficient.py
@@ -0,0 +1,4 @@
+
+def calc_prob(max_weight, num_of_cargos, cargo_mean_weight, cargo_sigma):
+
+calc_prob(9800, 49, 205, 15)
