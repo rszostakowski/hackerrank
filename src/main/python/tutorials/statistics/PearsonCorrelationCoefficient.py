9d133e29c3760c92e6e409a88fd2a15aab53c43e
rszostakowski <robertszostakowski@gmail.com>
2019-01-05 12:42:40 +0100

    pearson rank coeff

--- src/main/python/tutorials/statistics/PearsonCorrelationCoefficient.py
+++ src/main/python/tutorials/statistics/PearsonCorrelationCoefficient.py
@@ -1,4 +1,18 @@
+import math 
 
-def calc_prob(max_weight, num_of_cargos, cargo_mean_weight, cargo_sigma):
+def calc_prob(n, X, Y):
+    mi_x = sum(X)/n
+    mi_y = sum(Y)/n
+    ranks_x = calc_ranks(X)
+    ranks_y = calc_ranks(Y)
+    d_2 = sum(math.pow([(ranks_x[i]-ranks_y[i]) for i in range(n)],2))
 
-calc_prob(9800, 49, 205, 15)
+def calc_ranks(X):
+    ranks_x = []
+    print(sorted(X))
+    print(X)
+
+n = 10
+X = [10.0, 9.8, 8.0, 7.8, 7.7, 7.0, 6.0, 5.0, 4.0, 2.0]
+Y = [200.0, 44.0, 32.0, 24.0, 22.0, 17.0, 15.0, 12.0, 8.0, 4.0]
+calc_prob(n, X, Y)
