4e7f19df10c3994ae8a93dbc0f7a3138142cbb2e
rszostakowski <robertszostakowski@gmail.com>
2019-01-12 19:19:03 +0100

    working on multiple regression

--- src/main/python/tutorials/statistics/MultipleLinearRegression.py
+++ src/main/python/tutorials/statistics/MultipleLinearRegression.py
@@ -1,30 +1,22 @@
 import numpy as np
 
+
 def calculate_iqr(X, Y, X_test):
-    X = np.array(X,float)
-    Y = np.array(Y,float)
-    X_test = np.array(X_test,float)
+    X = np.array(X, float)
+    Y = np.transpose(np.array(Y, float))
+    X_test = np.array(X_test, float)
+    print(X)
+    print(Y)
+    print(X_test)
+    X_trans = np.transpose(X)
+    XX_inv = np.linalg.inv(np.matmul(X_trans, X))
+    B = np.matmul(np.matmul(XX_inv, X_trans), Y)
+    print(B)
+    print(np.matmul(X_test, B))
 
-    pass
 
-def calc_linear_reg(x_arr, y_arr):  
-    pass
+X = [[1, 5, 7], [1, 6, 6], [1, 7, 4], [1, 8, 5], [1, 9, 6]]
+Y = [10, 20, 60, 40, 50]
+X_test = [[1, 5, 5], [2, 5, 5]]
 
-def get_input_params():
-    N, m = list(map(int, input().split(" ")))
-    X = []
-    Y = []
-    X_test = []
-    for i in range(m):
-        raw = input().strip().split(' ')
-        X.append(raw[:m])
-        Y.append(raw[m:])
-    q = int(input().strip())
-    for i in range(q):
-        x_arr = input().strip().split(' ')
-        X_test.append(x_arr)
-    
-    return X, Y, X_test
-
-X, Y, X_test = get_input_params()    
 print(calculate_iqr(X, Y, X_test))
