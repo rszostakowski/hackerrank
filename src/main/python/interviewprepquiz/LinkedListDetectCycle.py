f4d53f52f7914f53f63fd8856624f9f9d640c42c
rszostakowski <robertszostakowski@gmail.com>
2019-01-15 21:36:18 +0100

    Detecting cycle

--- src/main/python/interviewprepquiz/LinkedListDetectCycle.py
+++ src/main/python/interviewprepquiz/LinkedListDetectCycle.py
@@ -1,1 +1,19 @@
-print("hello")
+class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+
+
+def has_cycle(head):
+    while(head!= None):
+        print("data {} next {}".format(head.data, head.next))
+        head = head.next
+    
+
+first = Node(11, None)
+fourth = Node(4, first)
+third = Node(5, fourth)
+second = Node(8, third)
+first = Node(12, second)
+
+has_cycle(first)
