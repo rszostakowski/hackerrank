aa4fa1a8c9cbe664cdaff6927a2ba39a2619fb81
rszostakowski <robertszostakowski@gmail.com>
2019-02-06 19:54:10 +0100

    still working on cleaning bot

--- src/main/python/ai/BotClean.py
+++ src/main/python/ai/BotClean.py
@@ -1,8 +1,5 @@
-import numpy as np
 
 def next_move(posr, posc, board):
-
-    board = np.array(board)
     dirt_x, dirt_y = find_closest_dirt(posr, posc, board)
     dist_x, dist_y = calc_dist(posr, posc, dirt_x, dirt_y)
 
@@ -23,20 +20,24 @@ def next_move(posr, posc, board):
 def find_closest_dirt(posr, posc, board):
     board_len = len(board)
 
+    coordinates = []
+    for i in range(-(board_len+1),board_len):
+        for j in range(-(board_len+1),board_len):
+            coordinates.append((i,j))
+
+    print(coordinates)
+    for i in coordinates:
+        new_x = posr + i[0]        
+        new_y = posc + i[1]        
+
+        if new_x >= 0 and new_x < board_len and new_y>= 0 and new_y < board_len and board[new_x][new_y] == 'd':
+
+            print(new_x, new_y)
+            return new_x, new_y
+    raise Exception("wrong")
 #  (1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1), (0,-1), (1,-1)
 #  (2,0), (2,1), (2,2), 
 # https://stackoverflow.com/questions/464864/how-to-get-all-possible-combinations-of-a-list-s-elements
-    for x in range(1,board_len):
-        print("x {}".format(x))
-        if posr+x >= 0 and posr+x < board_len and board[posr+x, posc] == 'd':
-            return posr+x, posc 
-        if posc+x >= 0 and posc+x < board_len and board[posr, posc+x] == 'd':
-            return posr, posc+x
-        if posr-x >= 0 and posr-x < board_len and board[posr-x, posc] == 'd':
-            return posr-x, posc
-        if posc-x >= 0 and posc-x < board_len and board[posr, posc-x] == 'd':
-            return posr, posc-x
-    return 1,1
 
 def calc_dist(m_x, m_y, p_x, p_y):
     dist_x=p_y - m_y
