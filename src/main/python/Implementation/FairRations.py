1ae46c489b9bd8b2af174d2c675cf03f2c6d0e46
rszostakowski <robertszostakowski@gmail.com>
2019-02-17 21:42:14 +0100

    coming back to problem solving

---
+++ src/main/python/Implementation/FairRations.py
@@ -0,0 +1,20 @@
+def fairRations(N, B):
+    print(N)
+    print(B)
+
+    suma = sum(B)
+    if (suma % 2 == 0)
+    print("NO")
+
+# find two odds neigbougrs 
+# find two odds with a even neigbour
+
+    odd_idx, odd_value = find_firt_odd(B)
+    B[add_idx]= odd_value + 1
+
+def find_firt_odd(B):
+    pass
+
+N = 2
+B = [1, 2]
+fairRations(N, B)
--- src/main/python/ai/MazeEscape.py
+++ src/main/python/ai/MazeEscape.py
@@ -1,26 +1,48 @@
+def solve_maze():
+    rd = int(input())
+    print("id: {}".format(id))
+    first =  input().strip()
+    second = input().strip()
+    third = input().strip()
+    print(first)
+    print(second)
+    print(third)
 
-id = int(input())
-print("id: {}".format(id))
-first =  input().strip().split()
-second = input().strip().split()
-third = input().strip().split()
-print(first)
-print(second)
-print(third)
+    upper = first[1]
+    left = second[0]
+    right = second[2] 
+    down = third[1]
 
-upper = first[1]
-left = second[0]
-right = second[2] 
-down = third[1]
-
-if upper != '#':
-    print("UP")
+    what_was_seen= createMatrix(5+5,5+5)
+    # find e in the input
+    # allocate to map 
+    # make a map larger to allocate the inputs
+    # memory of the bot?? how have it
+    # saving to file is a solution https://www.hackerrank.com/challenges/maze-escape/forum
+    print(what_was_seen)
     
-if left != '#':
-    print("LEFT")
+    if upper != '#':
+        save_to_file(
+        print("UP")
+        
+    if left != '#':
+        print("LEFT")
 
-if right != '#':
-    print("RIGHT")
+    if right != '#':
+        print("RIGHT")
 
-if down != '#':
-    print("DOWN")
+    if down != '#':
+        print("DOWN")
+
+def createMatrix(rowCount, colCount):
+    mat = []
+    for i in range(rowCount):
+        rowList = []
+        for j in range(colCount):
+            # you need to increment through dataList here, like this:
+            rowList.append(" ")
+        mat.append(rowList)
+
+    return mat
+
+solve_maze()
