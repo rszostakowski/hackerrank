6f5fe57f31293daa2fe2d4eadd8d6b2178143663
rszostakowski <robertszostakowski@gmail.com>
2019-11-09 10:40:49 +0100

    working on couting valleys

--- src/main/java/interview_kit/warm_up/CountingValleys.java
+++ src/main/java/interview_kit/warm_up/CountingValleys.java
@@ -7,10 +7,14 @@ public class CountingValleys {
     // Complete the sockMerchant function below.
     static int countingValleys(int n, String s) {
 
-        IntStream.range(0, n)
-                .forEach(System.out::println);
+        int counted = (int) IntStream.range(0, n-1)
+		.mapToObj(num -> s.substring(num, num+2))
+		.filter(str -> str.equals("UD") || str.equals("DU"))
+                // .forEach(k-> System.out.println(k));
+		.count();
 
-        return 0;
+	System.out.println(counted);
+        return counted;
     }
 
     public static void main(String[] args) {
